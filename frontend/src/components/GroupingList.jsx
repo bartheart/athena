1a52e7deecd253cb94cc829a98ca60adf6dbfd1d
bartheart <bemnetmerkebu17@gmail.com>
2024-05-22 15:02:10 -0500

    GroupingList component completed

--- frontend/src/components/GroupingList.jsx
+++ frontend/src/components/GroupingList.jsx
@@ -1,27 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../views/Dashboard/Dashboard.css';
 import axios from 'axios';
 
-function GroupingList() {
-    
+const GroupingList = () => {
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    const fetchAnswers = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/process_answers');
+        console.log("Fetched answers:", response.data); // Debug log
+        setAnswers(response.data);
+      } catch (error) {
+        console.error("Error fetching the answers:", error);
+      }
+    };
+
+    fetchAnswers();
+  }, []);
+
   return (
     <div className="submission-stats">
-        <div className="stats-header">First Group</div>
-        <div className="group-lists">
-        <div className="similarity-range">
-                <div className="description">0-25% Similarity</div>
-            </div>
-            <div className="similarity-range">
-                <div className="description">25-50% Similarity</div>
-            </div>
-            <div className="similarity-range">
-                <div className="description">50-75% Similarity</div>
-            </div>
-            <div className="similarity-range">
-                <div className="description">75-100% Similarity</div>
-            </div>
+      <div className="stats-header">First Group</div>
+      <div className="answer-list-container">
+        <div className="question">
+            <h1>#1. What is the purpose of prototype in software development?</h1>
+            <div className="points">5 Points</div>
         </div>
-    </div>  
+        <div className="answer-list">
+          {answers.map((answer, index) => (
+            <div key={index} className="answer-box">
+              {answer.answer}
+            </div>
+          ))}
+        </div>
+        
+      </div>
+    </div>
   );
 }
 
